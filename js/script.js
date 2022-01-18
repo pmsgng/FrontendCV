d285bce61dc4d09db27a260dde33e8f9cf9bee1c
Цой Влад <pmsgng88@gmail.com>
2022-01-18 22:04:38 +0300

    js animation

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const hero = document.querySelector('.hero');
+//     const foto = document.querySelector('.header__pic');
+//     window.addEventListener('scroll', () => {
+//         let scrollTop = window.scrollY;
+//         console.log(scrollTop);
+//         hero.style.transform = `translateX(${scrollTop*0.1}rem)`;
+//     });
+// });
+
