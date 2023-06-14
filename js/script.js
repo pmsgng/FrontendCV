8935b3d02ffa7d5145f90fb6c56d4f5487fa9195
Цой Влад <pmsgng88@gmail.com>
2023-06-14 11:29:22 +0300

    added html block with project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
 //     const hero = document.querySelector('.hero');
 //     const foto = document.querySelector('.header__pic');
@@ -9,5 +9,10 @@
 //         console.log(scrollTop);
 //         hero.style.transform = `translateX(${scrollTop*0.1}rem)`;
 //     });
-// });
+        $(window).scroll(function(){
+        if ( $(this).scrollTop() > el.offset().top - 200 ) {
+            el.addClass('animation_car8');
+    }
+});
+});
 
